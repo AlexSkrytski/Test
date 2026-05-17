96b5a76da7f476a301978cf760df96ffc68a4747
AlexSkrytski <alcoreservice@gmail.com>
2026-05-17 15:22:09 +0300

    Add files via upload

---
+++ GetAgeStatus/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GetAgeStatus
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int parsedAge = 0;
+            bool notValidAge = true; 
+
+            do
+            {
+                Console.WriteLine("Введите возраст");
+                string ageInputValue = Console.ReadLine() ?? string.Empty;
+                bool parseResult = int.TryParse(ageInputValue, out int age);
+
+                if (parseResult == false || age > 100 || age < 1)
+                {
+                    Console.WriteLine("Неверно, введите цифры от 1 до 100");
+                }
+                else
+                {
+                    notValidAge = false;
+                    parsedAge = age;
+                }
+            } while (notValidAge);
+
+            if (parsedAge >= 18)
+            {
+                Console.WriteLine("Человек совершеннолетний.");
+            }
+            else
+            {
+                Console.WriteLine("Человек несовершеннолетний.");
+            }
+        }
+    }
+}
